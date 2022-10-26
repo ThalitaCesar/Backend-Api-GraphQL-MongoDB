0fd578b7882f88a4ae5d5bf779cbfc7c49b41df1
ThalitaCesar <thalita.walleska@gmail.com>
2022-10-26 11:02:00 -0300

    api graphql

---
+++ src/models/fruit.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const FruitSchema = mongoose.Schema({
+   name: String,
+   nutritions: {
+      sugar: String,
+      calories: String,
+   },
+});
+module.exports = mongoose.model("Fruit", FruitSchema);
